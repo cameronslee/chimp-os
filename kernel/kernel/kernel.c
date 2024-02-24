aee44fb4b609535ecb01385c250d2628dd0344a7
cameron <cameronslee@hotmail.com>
2024-02-24 12:46:53 -0800

    ISR setup, bug with paging or stack corruption

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -48,13 +48,15 @@ extern void idt_load();
 
 void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags) 
 {
-    idt[num].base_lo = (base & 0xFFFF);
+    // apply bitmask to preserve lower 16 bits
+    idt[num].base_lo = (base & 0xFFFF); 
+    // shift 16 bits, apply bit mask for upper 16 bits for safety
     idt[num].base_hi = ((base >> 16) & 0xFFFF);
 
     idt[num].sel = sel;
+    idt[num].always0 = 0;
     idt[num].flags = flags;
 
-    idt[num].always0 = 0;
 }
 
 void idt_install() 
@@ -153,16 +155,155 @@ void gdt_install()
 }
 		
 
+/* ======== ISR HANDLES ======== */
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+void isrs_install()
+{
+    idt_set_gate(0, (unsigned)isr0, 0x08, 0x8E);
+    idt_set_gate(1, (unsigned)isr1, 0x08, 0x8E);
+    idt_set_gate(2, (unsigned)isr2, 0x08, 0x8E);
+    idt_set_gate(3, (unsigned)isr3, 0x08, 0x8E);
+    idt_set_gate(4, (unsigned)isr4, 0x08, 0x8E);
+    idt_set_gate(5, (unsigned)isr5, 0x08, 0x8E);
+    idt_set_gate(6, (unsigned)isr6, 0x08, 0x8E);
+    idt_set_gate(7, (unsigned)isr7, 0x08, 0x8E);
+
+    idt_set_gate(8, (unsigned)isr8, 0x08, 0x8E);
+    idt_set_gate(9, (unsigned)isr9, 0x08, 0x8E);
+    idt_set_gate(10, (unsigned)isr10, 0x08, 0x8E);
+    idt_set_gate(11, (unsigned)isr11, 0x08, 0x8E);
+    idt_set_gate(12, (unsigned)isr12, 0x08, 0x8E);
+    idt_set_gate(13, (unsigned)isr13, 0x08, 0x8E);
+    idt_set_gate(14, (unsigned)isr14, 0x08, 0x8E);
+    idt_set_gate(15, (unsigned)isr15, 0x08, 0x8E);
+
+    idt_set_gate(16, (unsigned)isr16, 0x08, 0x8E);
+    idt_set_gate(17, (unsigned)isr17, 0x08, 0x8E);
+    idt_set_gate(18, (unsigned)isr18, 0x08, 0x8E);
+    idt_set_gate(19, (unsigned)isr19, 0x08, 0x8E);
+    idt_set_gate(20, (unsigned)isr20, 0x08, 0x8E);
+    idt_set_gate(21, (unsigned)isr21, 0x08, 0x8E);
+    idt_set_gate(22, (unsigned)isr22, 0x08, 0x8E);
+    idt_set_gate(23, (unsigned)isr23, 0x08, 0x8E);
+
+    idt_set_gate(24, (unsigned)isr24, 0x08, 0x8E);
+    idt_set_gate(25, (unsigned)isr25, 0x08, 0x8E);
+    idt_set_gate(26, (unsigned)isr26, 0x08, 0x8E);
+    idt_set_gate(27, (unsigned)isr27, 0x08, 0x8E);
+    idt_set_gate(28, (unsigned)isr28, 0x08, 0x8E);
+    idt_set_gate(29, (unsigned)isr29, 0x08, 0x8E);
+    idt_set_gate(30, (unsigned)isr30, 0x08, 0x8E);
+    idt_set_gate(31, (unsigned)isr31, 0x08, 0x8E);
+}
+
+const char *exception_messages[] =
+{
+    "Division By Zero",
+    "Debug",
+    "Non Maskable Interrupt",
+    "Breakpoint",
+    "Into Detected Overflow",
+    "Out of Bounds",
+    "Invalid Opcode",
+    "No Coprocessor",
+
+    "Double Fault",
+    "Coprocessor Segment Overrun",
+    "Bad TSS",
+    "Segment Not Present",
+    "Stack Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Unknown Interrupt",
+
+    "Coprocessor Fault",
+    "Alignment Check",
+    "Machine Check",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved"
+};
+
+/* Stack architecture once ISR runs */
+struct regs
+{
+    unsigned int gs, fs, es, ds;      /* pushed the segs last */
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pushw' */
+    unsigned int int_no, err_code;    /* pushw #' and err code*/
+    unsigned int eip, cs, eflags, useresp, ss;   /* pushed by the processor automatically */ 
+};
+/*  
+ *  Upon fault, endless loop. 
+ *
+ *  All ISRs disable interrupts while they are being
+ *  serviced as a 'locking' mechanism to prevent an IRQ from
+ *  happening and messing up kernel data structures 
+*/
+
+void fault_handler(struct regs *r)
+{
+    if (r->int_no < 32)
+    {
+        puts(exception_messages[r->int_no]);
+        puts(" Exception. System Halted!\n");
+        for (;;);
+    }
+}
+
 /* ======== Kernel ======== */
 void kernel_main(void) {
     gdt_install();
     idt_install();
+    isrs_install();
     terminal_initialize();
     const char* d = "                               Welcome to Chimp OS\n";
-    /*
-    This should cause a system reset, need to handle with ISR
+    terminal_writestring((const char *) d);
+
+    //This should trigger a division by zero isr
     int foo = 5 / 0;
     terminal_writestring((const char *) foo);
-    */
 }
 
