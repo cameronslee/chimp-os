7ea855e54ae4ed694d59ddda9757e481aaceb928
cameron <cameronslee@hotmail.com>
2024-02-27 09:58:46 -0800

    keyboard handler, need to sync interrupts with sys timer

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -495,7 +495,7 @@ void timer_phase(int hz)
     outportb(0x40, divisor >> 8);     /* Set high byte of divisor */
 }
 
-int timer_ticks = 0;
+unsigned int timer_ticks = 0;
 
 void timer_wait(int ticks)
 {
@@ -536,6 +536,76 @@ void timer_install()
     irq_install_handler(0, timer_handler);
 }
 
+
+/* ======== Keyboard ======== */
+/* US Keyboard Layout */
+unsigned char kbdus[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+  '9', '0', '-', '=', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'q', 'w', 'e', 'r',	/* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+  'm', ',', '.', '/',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};		
+
+/* Handles the keyboard interrupt */
+void keyboard_handler(struct regs *r)
+{
+    unsigned char scancode;
+
+    /* Read from kb data buffer */
+    scancode = inportb(0x60);
+
+    /* If the top bit of the byte we read from kb  is
+    *  set, that means that a key has just been released */
+    if (scancode & 0x80)
+    {
+        // TODO handle shift, alt, ctrl
+    }
+    else
+    {
+        /* Handle key presses. If held , multiple interrupts will be issued */
+        puts("LOG");
+    }
+}
+
+/* Install keyboard handler into IRQ1 */
+void keyboard_install()
+{
+    irq_install_handler(1, keyboard_handler);
+}
+
 /* ======== Kernel ======== */
 void kernel_main(void) {
     gdt_install();
@@ -544,12 +614,16 @@ void kernel_main(void) {
     irq_install();
     timer_install();
 
-    __asm__ __volatile__ ("sti"); 
-
     terminal_initialize();
+    keyboard_install();
+
+    // TODO implement system timer
+
+    __asm__ __volatile__ ("sti"); 
 
     // wait 5 seconds
     timer_wait(500);
+
     const char* d = "                               Welcome to Chimp OS\n";
     terminal_writestring((const char *) d);
 
